c1e50556680bc794ee5591bdfee3cadaa3f7c5c6
Devilbat75 <112400858+Devilbat75@users.noreply.github.com>
2023-02-23 18:08:55 +0100

    MAJ panier

--- front/js/product.js
+++ front/js/product.js
@@ -30,35 +30,44 @@ function lesProduits(produit) {
   }
 }
 
-//ajouter des élements au panier
-const addToCart = document.getElementById("addToCart")
-addToCart.addEventListener("click", () => {
-  const addProduct = {
-    quantity: document.getElementById("quantity").value,
-    colors: document.getElementById("colors").value,
-    id: id
+// fonction d'affichage du produit de l'api
+function lesProduits(produit) {
+  let imageAlt = document.querySelector("article div.item__img");
+  let titre = document.querySelector("#title");
+  let prix = document.querySelector("#price");
+  let description = document.querySelector("#description");
+  let couleurOption = document.querySelector("#colors")
+
+  imageAlt.innerHTML = `<img src ="${produit.imageUrl}" alt= "${produit.altTxt}">`
+  titre.textContent = produit.name;
+  prix.textContent = produit.price;
+  description.textContent = produit.description;
+  for (let couleur of produit.colors) {
+    couleurOption.innerHTML += `<option value="${couleur}" > ${couleur} </option>`;
   }
+}
+
+let choixProduit = document.querySelector("#addToCart");
+choixProduit.addEventListener("click", async () => {  //on met async pour tuiliser await sur addPanier
+
+  let choixQuantite = document.querySelector("#quantity").value;
+  let choixCouleur = document.querySelector("#colors").value;
   if (
-    addProduct.quantity < 1 ||
-    addProduct.quantity > 100 ||
-    addProduct.quantity === undefined ||
-    addProduct.quantity == (Math.sign("-")) ||
-    addProduct.quantity == Number.parseFloat ||
-    addProduct.colors === "" ||
-    addProduct.colors === undefined
+    choixQuantite < 1 ||
+    choixQuantite > 100 ||
+    choixQuantite == (Math.sign("-")) ||
+    choixQuantite === undefined ||
+    choixCouleur === "" ||
+    choixCouleur === undefined
   ) {
-    alert("Pour valider le choix de votre article veuillez renseignez une couleur et / ou une quantité valide entre 1 et 100")
+    // active alert
+    alert("pour valider le chois de cet article, veuillez renseigner une couleur, et /ou une quantité valide entre 1 et 100")
   } else {
-    addProductLocalStorage = []
-    if (localStorage.getItem("addToCard") !== null) {
-      addProductLocalStorage = JSON.parse(localStorage.getItem("addToCart"))
-      addProductLocalStorage.push(addToCart)
-      localStorage.setItem("addToCart", JSON.stringify(addProductLocalStorage))
-      alert("Votre article a bien été ajouter au panier")
-    } else {
-      addProductLocalStorage.push(addProduct)
-      localStorage.setItem("addToCart", JSON.stringify(addProductLocalStorage))
-      alert("Votre article a bien été ajouter au panier")
-    }
+    articleClient.quantite = parseInt(choixQuantite);
+    articleClient.couleur = choixCouleur;
+    // montre panier 
+    await addPanier(articleClient);
+    alert("l'article a bien été ajouté au panier");
+    window.location.assign("cart.html")
   }
 })
